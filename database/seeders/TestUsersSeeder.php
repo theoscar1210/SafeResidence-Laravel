4c38cab65a8f93c9189c0aed510f9cb4a683231e
theoscar <oscardfer2020@gmail.com>
2026-03-03 15:47:34 -0500

    Seeder de usuarios de prueba para todos los roles

---
+++ database/seeders/TestUsersSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class TestUsersSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $users = [
+            [
+                'data' => [
+                    'first_name' => 'Oscar',
+                    'last_name'  => 'Fernandez',
+                    'cedula'     => '1152445689',
+                    'phone'      => '3172957831',
+                    'username'   => 'vigilante1',
+                    'email'      => 'vigilante@saferesidence.com',
+                    'password'   => Hash::make('Vigilante@123'),
+                ],
+                'role' => 'Vigilante',
+            ],
+            [
+                'data' => [
+                    'first_name' => 'Emma',
+                    'last_name'  => 'Naranjo',
+                    'cedula'     => '2222222',
+                    'phone'      => '3125555555',
+                    'username'   => 'propietario1',
+                    'email'      => 'propietario@saferesidence.com',
+                    'password'   => Hash::make('Propietario@123'),
+                ],
+                'role' => 'Propietario',
+            ],
+            [
+                'data' => [
+                    'first_name' => 'Maria',
+                    'last_name'  => 'Fernandez',
+                    'cedula'     => '123654789',
+                    'phone'      => '3176598745',
+                    'username'   => 'residente1',
+                    'email'      => 'residente@saferesidence.com',
+                    'password'   => Hash::make('Residente@123'),
+                ],
+                'role' => 'Residente',
+            ],
+        ];
+
+        foreach ($users as $item) {
+            $user = User::firstOrCreate(
+                ['username' => $item['data']['username']],
+                $item['data']
+            );
+            $user->assignRole($item['role']);
+        }
+    }
+}
